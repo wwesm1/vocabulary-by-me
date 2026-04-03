48ebae99d791ec6d02902a4d171a925ac0c7049c
wwesm1 <hulkaroynormurodova1234@gmail.com>
2026-04-03 15:37:37 +0500

    task 1

--- main.js
+++ main.js
@@ -854,3 +854,32 @@
 //     return deleteEnd
 // }
 // let output = array(1)
+
+let products = [
+    {
+        product: `Non`,
+        productPrice: 5000 
+    },
+    {
+        product: `Go'sht`,
+        productPrice: 120000,
+    },
+    {
+        product: `Banan`,
+        productPrice: 12000,
+    },
+    {
+        product: `Shokolad`,
+        productPrice: 14000
+    },
+    {
+        product: `Kofe`,
+        productPrice: 50000
+    }
+]
+
+products.forEach((productDiscount)=> {
+    let discount = productDiscount.productPrice * 0.10
+    let finalPrice = productDiscount.productPrice - discount
+    console.log(`Mahsulot nomi: ${productDiscount.product}\nMahsulotning haqiqiy narxi: ${productDiscount.productPrice}\nSiz uchun chegirma: ${discount}\nTo'lov: ${finalPrice}`)
+}) 
